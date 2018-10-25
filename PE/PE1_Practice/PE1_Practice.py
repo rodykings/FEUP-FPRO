84724cbb6b0b1c5b0a6ac4adc08b975ed4634f71
rodykings <rodrigocreis.12@gmail.com>
2018-10-25 18:38:04 +0100

    UPDATE

--- PE/PE1_Practice/PE1_Practice.py
+++ PE/PE1_Practice/PE1_Practice.py
@@ -1,22 +1,17 @@
+num = int(input("Input a number: "))
+second = 0
 
+if num < 0:
+    print("Its is impossible to find the square root")
+else:
+    first = num/2
+    second = (first+(num/first))/2
 
-lower_input = int(input("lower="))
-upper_input = int(input("upper="))
-
-
-def prime_numbers(lower, upper):
-    prime_str = ""
-    for i in range(lower, upper+1, 1):
-        divisors = 0
-        for n in range (1, i+1):
-            if i % n == 0:
-                divisors += 1
-        if divisors == 2:
-            prime_str += str(n) + " "
-    return prime_str
-
-final_str = "Prime numbers between " + str(lower_input) + " and " + str(upper_input) + " are: \n" + prime_numbers(lower_input, upper_input)
-print(final_str)
+    while abs(second-first) > 0.01:
+        first = second
+        second = (second+(num/second))/2
+   
+print("%.3f" % second)
 
 
             
--- PE/PE1_Practice/question3.py
+++ PE/PE1_Practice/question3.py
@@ -1,17 +1,15 @@
 
-import random
-
 num = int(input("Input a number: "))
 second = 0
 
 if num < 0:
     print("Its is impossible to find the square root")
 else:
-    first = random.uniform(0, num)
+    first = num/2
     second = (first+(num/first))/2
 
     while abs(second-first) > 0.01:
         first = second
         second = (second+(num/second))/2
    
-print(round(second, 3))
+print("%.3f" % second)
