ba0439724da3007916c6e12919342f8647633d63
rodykings <rodrigocreis.12@gmail.com>
2018-11-21 22:35:33 +0000

    UPDATE

---
+++ RE/RE08/manipulator.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 21 22:22:08 2018
+
+@author: Rodrigo Reis
+"""
+
+#Insert integer e at position idx
+def insert(l, idx, e):
+    l.insert(e, idx)
+    
+def print_f(l, )    
--- RE/RE08/matrix.py
+++ RE/RE08/matrix.py
@@ -3,19 +3,24 @@
 # 1 2|
 # 3 4|
 
-def mult(M, N):
+def matrix(M, N):
 
-    result = []
-
+    R = []
+    
+    for i in 
+    
     if len(M[0]) == len(N[0]):
-        
-        for i in range(0, len(M[0])+1):
-            result += []
-            
-            
+        #ITERATE M ROWS
+        for r_M in range(len(M)):
+            #ITERATE N COLLUMNS
+            for c_N in range(len(N[0])):
+                #ITERATE N ROWS:
+                for r_N in range(len(N)):
+                    R[r_M][c_N] += M[r_M][c_N] * N[r_N][c_N]
+        for r in R:
+            print(r)
+      
+    else:
+        return[]
 
-
-    return []
-a = [[7, 8, 9, 10]]
-
-
+matrix([[1, 2], [3, 4]], [[2, 0], [1, 2]])
