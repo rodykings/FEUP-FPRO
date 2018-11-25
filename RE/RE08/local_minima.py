5ff977c73439999ebea62b2908f9356d45bcbc37
rodykings <rodrigocreis.12@gmail.com>
2018-11-25 21:44:36 +0000

    RE8 UPDATES

--- RE/RE08/local_minima.py
+++ RE/RE08/local_minima.py
@@ -1,83 +1,28 @@
 
-
 def local_minima(alist, n):
-
-    final_tuple = ()
-        
-    neighbors = (n-1)//2 
+    n = (n//2)
+    result = []
+    last_value = -20
+    last_i = -20
     
-    for (i, value) in enumerate(alist):
-        #first in the list
-        if i == 0:
-            verify = True
-            for t in range(1, n):
-                if value > alist[t]:
-                    verify = False   
-                    break
-            if verify == True:
-                final_tuple += ((value, i),)
-            
-        #last in the list    
-        elif i == len(alist)-1:
-            verify = True
-            for t in range(len(alist)-n, len(alist)-1, -1):
-                if value > alist[t]:
-                    verify = False
-                    break
-            if verify == True:
-                final_tuple += ((value, i),)
-        
-        #if is in the middle     
+    for i in range(len(alist)):
+        current = alist[i]
+        if (i-n) < 0:                                          
+            another_list = alist[i:i+n+1]
+            if min(another_list) == current:                      
+                result.append((current, i))
+                last_value = current
+                last_i = i
         else:
-
-            verify = True
-            #neighborhood right
-            if  i + neighbors >= len(alist)-1:  
-                if i+1 == len(alist)-1:
-                    if value > alist[i+neighbors]:
-                        verify = False
+            another_list = alist[i-n:i+n+1]                         
+            if min(another_list) == current:
+                if last_value == current and last_i == i-1:
+                    last_value = current
+                    last_i = i
+                    pass
                 else:
-                    for t in range(i+1, len(alist)-1):
-                        if value > alist[t]:
-                            verify = False   
-                            break   
-            else:
-                if i+1 == i+neighbors:
-                    if value > i+neighbors:
-                        verify = False
-                else:
-                    for t in range(i+1, i+neighbors):
-                        if value > alist[t]:
-                            verify = False   
-                            break
-            
-            #neightborhood left
-            if  i - neighbors >= 0: 
-                
-                if i-1 == i-neighbors:
-                    if value > alist[t]:
-                        verify = False
-                else:
-                    for t in range(i-1, i-neighbors, -1):
-                        if value > alist[t]:
-                            verify = False   
-                            break
-            else:
-                for t in range(i-1, 0, -1):
-                    if value > alist[t]:
-                        print(value)
-                        verify = False   
-                        break
-               
+                    result.append((current, i))
+                    last_value = current
+                    last_i = i
     
-            if verify == True:
-                final_tuple += ((value, i),)
-        
-        #print(minimals)
-        #print(index)
-        #joining minimals and index
-    
-    return list(final_tuple)
-       
-print(local_minima([10, 3, 3, 14, 5, 7, 4], 3))
-print(local_minima([0, 3, 3, 14, 5, 7, 4], 3))
+    return result
